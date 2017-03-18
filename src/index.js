6905c186912d1ffa871b12a6e407b9534e1cd3ae
u509625 <Jonathan.Ford@wellsfargo.com>
2017-03-18 02:00:20 -0700

    Saving work. Start of YT search.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyCcbCQ9AfMh3-cuLaEzm4_UND3BmCJblO4';
+
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
+  console.log(data);
+});
 
 const App = () => {
-  return <div>Hi!</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
